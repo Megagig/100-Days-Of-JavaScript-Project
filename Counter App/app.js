96ec01500fe92ac3444b24d935925b96fc96bc44
Megagig <megagigsoftwaresolutions.com>
2023-03-09 19:10:54 +0100

    count maker

---
+++ Counter App/app.js
@@ -0,0 +1,18 @@
+//set initial count
+let count = 0;
+//select value and buttons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+// run a forEach method and callback function on the nodelist
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else {
+      count = 0;
+    }
+  });
+});
